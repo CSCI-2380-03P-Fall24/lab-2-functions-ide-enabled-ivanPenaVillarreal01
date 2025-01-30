8162ab5f736a5cdc372cb326f6515f219aa33824
ivanPenaVillarreal01 <ivan.penavillarreal01@utrgv.edu>
2025-01-30 11:36:21 -0600

    Update FunctionsLab.cpp

--- FunctionsLab.cpp
+++ FunctionsLab.cpp
@@ -11,7 +11,7 @@ using namespace std; // making life easier so that we do not need to use std::ci
 
 
 int locateSmallest(int array[], int start, int end){
-     if (start > end) {
+	if (start > end) {
         return -1; 
     }
 
@@ -32,8 +32,8 @@ int locateSmallest(int array[], int start, int end){
 //     an int which is the starting index of search,
 //     an int which is the ending point of searchspace
 // function: Search in the array from given start to end index and replace the given target value with 1000
-void replaceVariable(int array[], int target, int start, int end{
-      for (int i = start; i <= end; i++) {
+void replaceVariable(int array[], int target, int start, int end) {
+	for (int i = start; i <= end; i++) {
         if (array[i] == target) {
             array[i] = 1000;
         }
@@ -45,12 +45,16 @@ void replaceVariable(int array[], int target, int start, int end{
 //write a function called printArray to print out the elements of the given array
 //output: nothing
 //input: the array of integers and its size
-void printArray(int array[], int size) {
-     for (int i = 0; i < size; i++) {
-        cout << array[i] << " ";  // Print the current element followed by a space
+int sumOdds(int array[], int start, int end){
+
+	int sum = 0;
+	for (int i = start; i <= end; i++) {
+        if (array[i] % 2 != 0) {  
+            sum += array[i];  
+        }
     }
-    cout << endl; 
- }
+    return sum;
+}
 
 
 
